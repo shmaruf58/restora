e9883acafffd9ff644d6b74ad8774ebc4c9083f8
shmaruf58 <maruf414100masum@gmail.com>
2023-05-04 21:04:24 +0600

    css style

--- src/main.jsx
+++ src/main.jsx
@@ -39,9 +39,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
-
       <RouterProvider router={router} />
-      
     </AuthProvider>
   </React.StrictMode>
 );
